c7a68299fdfcf930d087a17929ad471d138bbd0e
yagocasasnovas <yago.casasnovas@gmail.com>
2019-01-16 18:52:46 +0100

    s

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
-//install node (nvm)
+#install nvm node version manager
+#https://nodesource.com/blog/installing-node-js-tutorial-using-nvm-on-mac-os-x-and-ubuntu/
+
+sudo apt-get install -y curl build-essential libssl-dev git
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
